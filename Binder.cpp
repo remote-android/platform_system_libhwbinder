93a7076efc37246a188e5759429ce35da1f04ab7
Ziyang Zhou <ziyang.zhou@outlook.com>
2021-05-09 23:44:12 +0800

    fix booting
    Change-Id: Ic154b6301b6fd4354f45efccc9375a06a9084e0b

--- Binder.cpp
+++ Binder.cpp
@@ -96,6 +96,7 @@ bool BHwBinder::isRequestingSid() {
 }
 
 void BHwBinder::setRequestingSid(bool requestingSid) {
+    requestingSid = false; // HACKED
     Extras* e = mExtras.load(std::memory_order_acquire);
 
     if (!e) {
